5ddb6c2afab755a13a1f1a2324dae510f8b0427d
Biguri A <ab2d17@soton.ac.uk>
2018-02-14 12:01:45 +0000

    Add node value interpolation v3

--- Utilities/meshInterpolateNodes.m
+++ Utilities/meshInterpolateNodes.m
@@ -1,9 +1,11 @@
 function [nodevals,grad]=meshInterpolateNodes(graph,trivals)
 
 graph=fixgraph(graph);
+nD=size(graph.elements(1).nodeId,2)-1;
+assert(nD==2 || nD==3,'Only 2D or 3D meshes supported');
 
 if nargin ==2
-   grad=zeros(size(graph.nodes,2),nD); 
+    grad=zeros(size(graph.nodes,2),nD);
 end
 % We are going to need the centroids of the elements
 centroids=zeros(size(graph.elements,2),nD);
@@ -18,20 +20,13 @@ for ii=1:size(graph.nodes,2)
     dsq=zeros(length(graph.nodes(ii).neighbour_elems),1);
     for jj=1:length(graph.nodes(ii).neighbour_elems)
         diri(jj,1:nD)=centroids(graph.nodes(ii).neighbour_elems(jj),:)-graph.nodes(ii).positions;
-        if strcmpi(method,'Sobel')
-            dsq(jj)=sum((centroids(graph.nodes(ii).neighbour_elems(jj),:)-graph.nodes(ii).positions).^2,2);
-        end
+        
     end
     gradi=trivals(graph.nodes(ii).neighbour_elems);
-    if strcmpi(method,'Sobel')
-        aux=lscov(diri,gradi,1./dsq);
-    elseif strcmpi(method,'Prewitt')
-        aux=diri\gradi;
-    end
+    aux=diri\gradi;
     if nargin ==2
         grad(ii,:)=aux(1:nD);
     end
-    nodevals(ii)=aux(end);
+    nodevals(ii,1)=aux(end);
 end
-
 end
