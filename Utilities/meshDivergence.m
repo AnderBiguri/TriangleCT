4dd311769ad7c05ce55dd25a4a82fed6f400e826
Biguri A <ab2d17@soton.ac.uk>
2018-02-14 17:21:23 +0000

    fix divergence bug namechange

--- Utilities/meshDivergence.m
+++ Utilities/meshDivergence.m
@@ -4,16 +4,18 @@ function div=meshDivergence(graph,nodevects)
 
 nD=size(graph.elements(1).nodeId,2)-1;
 assert(nD==2 || nD==3,'Only 2D or 3D meshes supported');
-
-divX=meshgradient(graph,nodevects(:,1));
-divX=divX(:,1);
-divY=meshgradient(graph,nodevects(:,2));
-divY=divY(:,2);
-
 divZ=[];
+% X
+divX=meshGradient(graph,nodevects(:,1));
+divX=divX(:,1);
+%Y
+divY=meshGradient(graph,nodevects(:,2));
+divY=divY(:,2);
+%Z
 if nD==3
-  divZ=meshgradient(graph,nodevects(:,3));
+  divZ=meshGradient(graph,nodevects(:,3));
   divZ=divZ(:,3);  
 end
+% divergence
 div=sum([divX divY divZ],2);
 end
