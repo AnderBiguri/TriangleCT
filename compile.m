edb3acbbdee755d60dc8183cec184b6adc8d3f16
ab2d17 <ab2d17@UOS-215399.soton.ac.uk>
2018-10-22 18:33:55 +0100

    update compile

--- compile.m
+++ compile.m
@@ -1,4 +1,3 @@
 clear mex
-mex -largeArrayDims ./Source/graphForward.cpp  ./Source/graph_ray_projection.cpp -outdir ./Mex
-
-mex -largeArrayDims ./Source/graphBackward.cpp  ./Source/ray_back_brute.cu -outdir ./Mex
+mex -largeArrayDims ./Source/graphForward.cpp  ./Source/graph_ray_projection.cu -outdir ./Mex
+mex -largeArrayDims ./Source/graphBackward.cpp  ./Source/graph_ray_backprojection.cu -outdir ./Mex
